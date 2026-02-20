0d4a29a668aaef16a122620cf9300456385adc88
Mark Tyrkba <awfulmarco@gmail.com>
2026-02-21 02:03:11 +0300

    fix: include lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    // clippy::cargo,
+    dead_code
+)]
+#![allow(
+    clippy::inline_always,
+    clippy::uninlined_format_args, // ?...
+    clippy::borrow_as_ptr,
+    clippy::collapsible_if,
+    clippy::new_without_default,
+    clippy::redundant_field_names,
+    clippy::struct_field_names,
+    clippy::ptr_as_ptr,
+    clippy::missing_transmute_annotations,
+    clippy::multiple_crate_versions,
+    clippy::cast_precision_loss,
+    clippy::cast_possible_truncation,
+    clippy::similar_names,
+    clippy::cast_sign_loss,
+    clippy::cast_possible_wrap,
+    clippy::used_underscore_binding,
+    clippy::nonstandard_macro_braces,
+    clippy::used_underscore_items,
+    clippy::enum_glob_use,
+    clippy::cast_lossless,
+    clippy::match_same_arms,
+    clippy::too_many_lines,
+    clippy::unnested_or_patterns,
+    clippy::blocks_in_conditions,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+)]
+
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+pub mod hash;
+pub mod object;
+pub mod store;
+pub mod wire;
+pub mod storage;
+pub mod repository;
+pub mod hash_object;
+pub mod cat_file;
+pub mod write_tree;
+pub mod commit;
+pub mod log;
+pub mod checkout;
+pub mod add;
+pub mod index;
+pub mod branch;
+pub mod cache;
+pub mod ignore;
+pub mod status;
+pub mod remove;
+pub mod util;
+pub mod tracy;
+pub mod tree;
